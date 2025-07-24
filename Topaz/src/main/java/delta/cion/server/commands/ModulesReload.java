67ce833e54e8310911e1670d15f82b2d2a04545f
Nionim <delta.cion.fe@gmail.com>
2025-07-25 00:39:05 +0300

    Code refactoring

--- Topaz/src/main/java/delta/cion/server/commands/ModulesReload.java
+++ Topaz/src/main/java/delta/cion/server/commands/ModulesReload.java
@@ -9,8 +9,12 @@ import net.minestom.server.command.builder.CommandContext;
 import net.minestom.server.command.builder.CommandExecutor;
 import net.minestom.server.command.builder.arguments.ArgumentType;
 import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ModulesReload extends Command implements CommandExecutor {
+
+	private final Logger LOGGER = LoggerFactory.getLogger("RELOAD_COMMAND");
 
 	public ModulesReload() {
 		super("reload");
@@ -20,13 +24,13 @@ public class ModulesReload extends Command implements CommandExecutor {
 	@Override
 	public void apply(@NotNull CommandSender sender, @NotNull CommandContext context) {
 		String moduleID = context.get("module-id");
+		LOGGER.debug("ModuleID: {}", moduleID);
 		if (moduleID == null || moduleID.isBlank()) {
 			Sender.send(sender, "Reloading all modules..");
 			Controller.reloadAll();
-			return;
 		}
-		if (!PluginLoader.getPluginIDS().contains(moduleID)) Sender.send(sender, "Unknown plugin");
-		if (Controller.reloadPlugin(moduleID)) Sender.send(sender, "Module {} reloaded!", moduleID);
+		else if (!PluginLoader.getPluginIDS().contains(moduleID)) Sender.send(sender, "Unknown plugin");
+		else if (Controller.reloadPlugin(moduleID)) Sender.send(sender, "Module {} reloaded!", moduleID);
 		else Sender.send(sender, "Module {} cant be reloaded!", moduleID);
 	}
 }
--- Topaz/src/main/java/delta/cion/server/plugins/utils/ValidatePlugin.java
+++ Topaz/src/main/java/delta/cion/server/plugins/utils/ValidatePlugin.java
@@ -8,7 +8,6 @@ import org.yaml.snakeyaml.Yaml;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.List;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
